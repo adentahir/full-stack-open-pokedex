e2f5c7867c81ae27c15a8f8611aee930d74aa9ec
Aden Tahir <aden.tahir7@gmail.com>
2023-08-15 19:22:53 +0500

    super duper small twitch

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,9 @@
+/* eslint-disable no-unused-vars */
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(_on, _config) {
+    setupNodeEvents(on, config) {
     },
-    baseUrl: 'http://localhost:3000',
   },
 })
